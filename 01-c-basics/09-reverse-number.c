821e02b2789f091f1b40f5f494ab14135c9fa242
Aary Potdar <aarypotdar01@gmail.com>
2026-08-22 11:48:32 +0530

    Add program to reverse an integer's digits
    This program accepts an integer and reverses its digits using a while loop, modulus operator, and integer division.

---
+++ 01-c-basics/09-reverse-number.c
@@ -0,0 +1,29 @@
+/*
+ * Problem: Reverse a number
+ * Concept: While loop, modulus operator and integer division
+ *
+ * Description:
+ * This program accepts an integer and reverses its digits
+ * using the modulus operator and integer division.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num, reversed = 0, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+
+    printf("Reversed number = %d\n", reversed);
+
+    return 0;
+}
