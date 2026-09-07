ed1fe17f1283f86bdf6e179365ae6663bec0d059
Aary Potdar <aarypotdar01@gmail.com>
2026-09-07 13:03:59 +0530

    Implement swap function using pointers in C
    This program swaps two numbers using pointers in C.

---
+++ 06-pointers/36-swap-using-pointers.c
@@ -0,0 +1,35 @@
+/*
+ * Problem: Swap two numbers using pointers
+ * Concept: Pointers, addresses and dereferencing
+ *
+ * Description:
+ * This program swaps two numbers using pointers.
+ * The swap is performed inside a function.
+ */
+
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(void)
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
